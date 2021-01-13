0d86815aa5570d1635b6a14324dbc9ea20d7758b
Nathan Castaldi <nathan@castaldi.dev>
2021-01-13 15:42:53 -0500

    declared teamArray as empty array

--- app.js
+++ app.js
@@ -10,9 +10,14 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+let teamArray = [];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+
+
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
